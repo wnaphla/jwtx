d6765d95752678f2d18e6e2fda087959b9378f86
wnaphla <1016192517@qq.com>
2020-05-23 14:40:45 +0800

    wn maven依赖

--- jwtx-web/src/test/java/com/wn/JwtxWebApplicationTests.java
+++ jwtx-web/src/test/java/com/wn/JwtxWebApplicationTests.java
@@ -1,10 +1,19 @@
 package com.wn;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
-class JwtxWebApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试
+@SpringBootTest(classes = com.wn.JwtxWebApplication.class,webEnvironment= SpringBootTest.WebEnvironment.NONE)
+//@ActiveProfiles({ProfileConstants.DEV,ProfileConstants.JUNIT})
+@Transactional
+//@Rollback(value = true)
+ public class JwtxWebApplicationTests {
 
     @Test
     void contextLoads() {
